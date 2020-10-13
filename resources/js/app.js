dcab7e56db29a847c70daa0f74f819a530c42efa
Saepul Bahri <aep.nat@gmail.com>
2020-10-13 15:30:03 +0700

    login and register view

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,12 @@
 import './bootstrap';
 import Vue from 'vue';
 import apolloProvider from './apollo.config';
-import Board from './pages/Board';
+import router from "./router.config";
 
 window.Vue = Vue;
 
-Vue.component('Board', Board);
-
 const app = new Vue({
     el: '#app',
-    apolloProvider
+    apolloProvider,
+    router
 });
---
+++ resources/js/router.config.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+import Board from "./pages/Board";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+Vue.use(VueRouter);
+
+const routes = [
+    { path: '/', name: "board", component: Board},
+    { path: '/login', name:"login", component: Login},
+    { path: '/register', name:"register", component: Register}
+];
+
+export default new VueRouter({
+    routes,
+    mode: 'history'
+})
